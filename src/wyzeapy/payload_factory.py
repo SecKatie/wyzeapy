147f74131c7b78686777cc77b1548c7aed518f41
Joe Schubert <joseph.schubert@gmail.com>
2023-01-07 12:57:12 -0800

    feat: fix lock querying - update access_token (#57)

--- src/wyzeapy/payload_factory.py
+++ src/wyzeapy/payload_factory.py
@@ -12,7 +12,7 @@ from .crypto import ford_create_signature
 
 def ford_create_payload(access_token: str, payload: Dict[str, Any],
                         url_path: str, request_method: str) -> Dict[str, Any]:
-    payload["accessToken"] = access_token
+    payload["access_token"] = access_token
     payload["key"] = FORD_APP_KEY
     payload["timestamp"] = str(int(time.time() * 1000))
     payload["sign"] = ford_create_signature(url_path, request_method, payload)

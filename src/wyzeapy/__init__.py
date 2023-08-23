1fc8cdb6aebc34aeac6764fd82d0fbaecfd3ce75
Brian Rogers <brg468@hotmail.com>
2023-08-23 10:44:13 -0400

    update refresh token

--- src/wyzeapy/__init__.py
+++ src/wyzeapy/__init__.py
@@ -83,8 +83,8 @@ class Wyzeapy:
                 email, password, key_id, api_key, token, self.execute_token_callbacks
             )
             if token:
-                # User token supplied, lets go ahead and use it and refresh the access token if needed.
-                await self._auth_lib.refresh_if_should()
+                # User token supplied, refresh on startup
+                await self._auth_lib.refresh()
             else:
                 await self._auth_lib.get_token_with_username_password(
                     email, password, key_id, api_key

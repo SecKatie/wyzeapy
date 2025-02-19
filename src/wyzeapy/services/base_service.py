98794d7b2464a50af37abb835db00880e1f7c733
blockarchitech <hello@blockarchitech.com>
2025-02-19 13:40:24 -0700

    chore: fix python-black goof

--- src/wyzeapy/services/base_service.py
+++ src/wyzeapy/services/base_service.py
@@ -29,7 +29,7 @@ class BaseService:
     _devices: Optional[List[Device]] = None
     _last_updated_time: time = 0  # preload a value of 0 so that comparison will succeed on the first run
     _min_update_time = 1200  # lets let the device_params update every 20 minutes for now. This could probably reduced signicficantly.
-    _update_lock: asyncio.Lock() = asyncio.Lock()
+    _update_lock: asyncio.Lock = asyncio.Lock() # fmt: skip
     _update_manager: UpdateManager = UpdateManager()
     _update_loop = None
     _updater: DeviceUpdater = None
